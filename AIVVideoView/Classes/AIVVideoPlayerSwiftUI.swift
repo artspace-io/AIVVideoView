827986ab1126cb4d81c8055b9a7d2ad5b07524b6
yitongtong <enamourchen@outlook.com>
2026-07-15 16:09:51 +0800

    change swiftui

--- AIVVideoView/Classes/AIVVideoPlayerSwiftUI.swift
+++ AIVVideoView/Classes/AIVVideoPlayerSwiftUI.swift
@@ -5,12 +5,8 @@ import AVFoundation
 public struct AIVVideoPlayerSwiftUI: UIViewRepresentable {
     @ObservedObject public var player: AIVVideoPlayer
 
-    /// videoGravity 现在是 AIVVideoPlayer 自身的状态（player.videoGravity），这里只是一个可选的初始化便利参数
-    public init(player: AIVVideoPlayer, videoGravity: AVLayerVideoGravity? = nil) {
+    public init(player: AIVVideoPlayer) {
         self.player = player
-        if let videoGravity {
-            player.videoGravity = videoGravity
-        }
     }
 
     public func makeUIView(context: Context) -> AIVVideoPlayerView {
